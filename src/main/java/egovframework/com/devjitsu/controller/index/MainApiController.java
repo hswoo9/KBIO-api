7ddd4f5b41cfdf518e39fb04336b74f56c731b1c
deer <deer@dev-jitsu.com>
2025-02-06 17:26:44 +0900

    1. 배너팝업 dto 추가

--- src/main/java/egovframework/com/devjitsu/controller/index/MainApiController.java
+++ src/main/java/egovframework/com/devjitsu/controller/index/MainApiController.java
@@ -3,6 +3,7 @@ package egovframework.com.devjitsu.controller.index;
 import egovframework.com.cmm.ResponseCode;
 import egovframework.com.cmm.service.EgovFileMngUtil;
 import egovframework.com.cmm.service.ResultVO;
+import egovframework.com.devjitsu.model.common.SearchDto;
 import egovframework.com.devjitsu.service.common.CommonApiService;
 import egovframework.com.devjitsu.service.main.MainApiService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,8 +27,9 @@ public class MainApiController {
     @Autowired
     private MainApiService mainApiService;
 
-    @PostMapping("/mainApi/getPopUpList")
-    public ResultVO getPopUpList() {
-        return mainApiService.getPopupList();
+    @PostMapping("/mainApi/getBnrPopupList.do")
+    public ResultVO getBnrPopupList(HttpServletRequest request) {
+        SearchDto dto = (SearchDto) request.getAttribute("searchDto");
+        return mainApiService.getBnrPopupList(dto);
     }
 }
