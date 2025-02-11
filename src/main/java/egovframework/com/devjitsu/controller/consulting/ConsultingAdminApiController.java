5543512678cc78eeaa51bbeffa1b28a479391f64
deer <deer@dev-jitsu.com>
2025-02-11 17:59:42 +0900

    1. 관리자 컨설팅 진행중

---
+++ src/main/java/egovframework/com/devjitsu/controller/consulting/ConsultingAdminApiController.java
@@ -0,0 +1,35 @@
+package egovframework.com.devjitsu.controller.consulting;
+
+import egovframework.com.cmm.service.ResultVO;
+import egovframework.com.devjitsu.model.common.SearchDto;
+import egovframework.com.devjitsu.model.consult.TblCnsltAply;
+import egovframework.com.devjitsu.model.consult.TblCnsltDtl;
+import egovframework.com.devjitsu.service.consult.ConsultingAdminApiService;
+import egovframework.com.devjitsu.service.consult.ConsultingApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class ConsultingAdminApiController {
+
+    @Autowired
+    private ConsultingAdminApiService consultingAdminApiService;
+
+
+    /**
+     * 컨설팅 리스트 조회
+     * @return
+     */
+    @PostMapping("/consultingApi/getConsultingList.do")
+    public ResultVO getComCdGroupList(HttpServletRequest request) {
+        SearchDto dto = (SearchDto) request.getAttribute("searchDto");
+        return consultingAdminApiService.getConsultingList(dto);
+    }
+}
