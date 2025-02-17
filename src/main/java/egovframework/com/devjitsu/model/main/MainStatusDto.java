cf0dbb5f890ecbf3800912da0a368d5c1d8bf89a
deer <deer@dev-jitsu.com>
2025-02-17 14:13:43 +0900

    1. 관리자 메인 달력 제외 완료 2. 게시글 상세보기 사용자 정보 추가 조회

---
+++ src/main/java/egovframework/com/devjitsu/model/main/MainStatusDto.java
@@ -0,0 +1,23 @@
+package egovframework.com.devjitsu.model.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainStatusDto {
+
+    private long mbrType1Cnt;
+    private long mbrType2Cnt;
+    private long mbrType3Cnt;
+    private long mbrType4Cnt;
+    private long dfclCnt;
+    private long cnsltAply26Cnt;
+    private long cnsltAply27Cnt;
+
+    private long activeYCnt;
+    private long activeWCnt;
+    private long activeRCnt;
+    private long activeSCnt;
+}
