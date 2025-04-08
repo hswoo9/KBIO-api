da98806dd131e528a4967c95a7b0e95ea8892df3
hswoo9 <gksthe@naver.com>
2025-04-08 15:20:55 +0900

    MOU기업 테이블 변경

--- src/main/java/egovframework/com/devjitsu/model/user/TblMou.java
+++ src/main/java/egovframework/com/devjitsu/model/user/TblMou.java
@@ -26,13 +26,45 @@ public class TblMou {
     @Comment("기관명")
     private String mouNm;
 
-    @Column(name = "MOU_CLBR_CLSF", length = 100)
-    @Comment("협력분류")
-    private String mouClbrClsf;
-
     @Column(name = "MOU_JOIN_YMD", columnDefinition = "DATETIME")
     @Comment("가입일")
     private LocalDateTime mouJoinYmd;
+
+    @Column(name = "RPRSV_NM", length = 100)
+    @Comment("대표자명")
+    private String rpsvNm;
+
+    @Column(name = "BRNO", columnDefinition = "CHAR(10)")
+    @Comment("사업자등록번호")
+    private String brno;
+
+    @Column(name = "BZENTY_EML_ADDR", length = 320)
+    @Comment("업체이메일주소")
+    private String bzentyEmlAddr;
+
+    @Column(name = "MOU_CLSF", length = 20)
+    @Comment("기업분류")
+    private String mouClsf;
+
+    @Column(name = "MOU_TPBIZ", length = 20)
+    @Comment("기업업종")
+    private String mouTpbiz;
+
+    @Column(name = "MOU_ADDR", length = 200)
+    @Comment("기업주소")
+    private String mouAddr;
+
+    @Column(name = "MOU_TELNO", length = 11)
+    @Comment("기업전화번호")
+    private String mouTelno;
+
+    @Column(name = "ZIP", columnDefinition = "CHAR(5)")
+    @Comment("우편번호")
+    private String zip;
+
+    @Column(name = "HMPG_ADDR", length = 2000)
+    @Comment("누리집주소")
+    private String hmpgAddr;
 
     @Column(name = "CREATR_SN", columnDefinition = "INT(10)", updatable = false, nullable = false)
     @Comment("생성자일련번호")
